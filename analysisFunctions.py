2b580e87988b8c98b400b8e78831b10014677d15
eholleman <estherholleman@gmail.com>
2017-02-09 09:50:36 +0100

    created calcCorrectRT function to calculate correct and incorrect when not used in a loop (to be used with a groupby & apply)

--- analysisFunctions.py
+++ analysisFunctions.py
@@ -5,25 +5,37 @@ Created on Thu Jan 12 12:19:32 2017
 
 @author: esther
 """
-
+import pandas as pd
 from preprocessingFunctions import makeRewards
-from preprocessingFunctions import calcCorrect
 
 
 
+
+def calcCorrectRT(df, r = 2, p = 1):
+        
+    phase = df.index.get_level_values(0).unique()[0]
+    
+    if phase > p:
+        r = r - 1
+     
+    rew = df.xs('reward_size',level = 0, axis = 1) 
+    add_rew = df.xs('additional_reward',level = 0, axis = 1)
+    valid =  df.xs('valid',level = 0, axis = 1)
+    
+    correct = (rew[valid] + add_rew[valid]) > r 
+    incorrect = (rew[valid] + add_rew[valid]) <= r
+
+    return pd.concat([correct,incorrect], axis = 1, keys =['correct','incorrect'])
+    
 def testRTbasedOnPhaseScoring(Adat,Mdat):
     
     Rewards = makeRewards(Adat,Mdat)   
-    
-    correct, incorrect, valid = calcCorrect(Rewards)
+
+    masks = Rewards.groupby(level =  ["Phase","Day"]).apply(calcCorrectRT)
     
     # extract reaction times
     rt = Adat.xs('reaction_time',level = 1, axis = 1)
-    
-    # and remove trials where experimenter gave reward before sensors were activated (0) 
-    # or sensor activated by tail (anything less than 200ms)
-    # That is, take only those trials with reaction times longer than 200ms
     rtFiltered = rt[rt > 200 ]
     
-    return rtFiltered,correct,incorrect,valid
+    return rtFiltered, masks.correct, masks.incorrect
      
