349e8164aab68cf2d9f26fa045453c8b124939bf
eholleman <estherholleman@gmail.com>
2019-06-25 13:28:45 +0200

    forgot to update git with notebooks and paper figures from last year

--- old/averageScores.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Sep  5 11:59:20 2016
-
-@author: esther
-"""
-
-averagedEveryThirdDay = averageScoresAllScores.groupby(averageScoresAllScores.index/3).mean();
-averagedEveryFifthDay = averageScoresAllScores.groupby(averageScoresAllScores.index/5).mean();
